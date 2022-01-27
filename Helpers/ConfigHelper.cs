44853ca23c44e15de6eec264c967623b9a878f24
oatsfx <ryanwgoreaz@gmail.com>
2022-01-26 23:13:00 -0700

    removed the ability to refresh users on bot startup
    this inadvertently causes some wipes depending on the bungie API responses

--- Helpers/ConfigHelper.cs
+++ Helpers/ConfigHelper.cs
@@ -44,31 +44,12 @@ namespace DestinyUtility.Helpers
             {
                 string json = File.ReadAllText(ActiveConfig.FilePath);
                 aConfig = JsonConvert.DeserializeObject<ActiveConfig>(json);
-
-                try
-                {
-                    foreach (ActiveConfig.ActiveAFKUser aau in ActiveConfig.ActiveAFKUsers)
-                    {
-                        int updatedLevel = DataConfig.GetUserSeasonPassLevel(aau.DiscordID, out int updatedProgression);
-                        aau.LastLevelProgress = updatedProgression;
-                        aau.LastLoggedLevel = updatedLevel;
-                    }
-                }
-                catch
-                {
-                    DataConfig.UpdateUsersList();
-                    Console.WriteLine($"[{String.Format("{0:00}", DateTime.Now.Hour)}:{String.Format("{0:00}", DateTime.Now.Minute)}:{String.Format("{0:00}", DateTime.Now.Second)}] Bungie API is down, loading stored data and continuing.");
-                }
-
-
-                string output = JsonConvert.SerializeObject(aConfig, Formatting.Indented);
-                File.WriteAllText(ActiveConfig.FilePath, output);
             }
             else
             {
                 aConfig = new ActiveConfig();
                 File.WriteAllText(ActiveConfig.FilePath, JsonConvert.SerializeObject(aConfig, Formatting.Indented));
-                Console.WriteLine($"No activeConfig.json file detected. A new one has been created and the program has stopped. Go and change API tokens and other items.");
+                Console.WriteLine($"No activeConfig.json file detected. A new one has been created and the program has stopped. No action is needed.");
                 closeProgram = true;
             }
 
